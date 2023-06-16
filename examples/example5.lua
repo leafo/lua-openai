20f7fc6d28d14fbb483fcc6543b6ad4bf7807e1d
leaf corcoran <leafot@gmail.com>
2023-06-16 09:48:31 -0700

    add sum function, make logging a bit better

--- examples/example5.lua
+++ examples/example5.lua
@@ -31,7 +31,7 @@ local two_numbers = {
 
 local chat = client:new_chat_session({
   temperature = 0,
-  -- model = "gpt-3.5-turbo-0613",
+  -- model = "gpt-3.5-turbo-0613", -- In my testing, gpt-3.5 is unable to figure it out
   model = "gpt-4-0613",
   messages = {
     {
@@ -40,9 +40,23 @@ local chat = client:new_chat_session({
     }
   },
   functions = {
-    { name = "add", description = "Add two numbers together", parameters = two_numbers },
-    { name = "divide", description = "Divide two numbers", parameters = two_numbers },
-    { name = "multiply", description = "Multiply two numbers together", parameters = two_numbers },
+    { name = "add", description = "Add two numbers, a + b", parameters = two_numbers },
+    { name = "subtract", description = "Subtract two numbers, a - b", parameters = two_numbers },
+    { name = "divide", description = "Divide two numbers, a / b", parameters = two_numbers },
+    { name = "multiply", description = "Multiply two numbers together, a * b", parameters = two_numbers },
+
+    {
+      name = "sum", description = "Add a list of numbers together",
+      parameters = {
+        type = "object",
+        properties = {
+          numbers = {
+            type = "array",
+            items = { type = "number" }
+          }
+        }
+      }
+    },
     {
       name = "sqrt", description = "Calculate square root of a number",
       parameters = {
@@ -62,20 +76,19 @@ function chat:send(v, ...)
   return chat_send(self, v, ...)
 end
 
-
-local one_args = types.annotate(types.string / cjson.decode * types.partial({
-  a = types.number
-}))
-
-local two_args = types.annotate(types.string / cjson.decode * types.partial({
+local two_args = types.string / cjson.decode * types.partial({
   a = types.number,
   b = types.number
-}))
+})
 
 local funcs = {
   add = {
     arguments = two_args,
     call = function(args) return args.a + args.b end
+  },
+  subtract = {
+    arguments = two_args,
+    call = function(args) return args.a - args.b end
   },
   divide = {
     arguments = two_args,
@@ -85,8 +98,22 @@ local funcs = {
     arguments = two_args,
     call = function(args) return args.a * args.b end
   },
+  sum = {
+    arguments = types.string / cjson.decode * types.partial({
+      numbers = types.array(types.number)
+    }),
+    call = function(args)
+      local sum = 0
+      for _, number in ipairs(args.numbers) do
+        sum = sum + number
+      end
+      return sum
+    end
+  },
   sqrt = {
-    arguments = one_args,
+    arguments = types.string / cjson.decode * types.partial({
+      a = types.number
+    }),
     call = function(args)
       return math.sqrt(args.a)
     end
@@ -99,7 +126,7 @@ while true do
   local last_message = chat:last_message()
 
   for k, v in pairs(last_message) do
-    p(k, v)
+    p("<<", k, v)
   end
 
   -- stop if no functions are requested
@@ -113,7 +140,7 @@ while true do
   if not func_handler then
     assert(chat:send("You called a function that is not declared: " .. func.name))
   else
-    local arguments, err = func_handler.arguments:transform(func.arguments)
+    local arguments, err = types.annotate(func_handler.arguments):transform(func.arguments)
     if not arguments then
       assert(chat:send("Invalid arguments for function " .. func.name .. ": " .. err))
     else
